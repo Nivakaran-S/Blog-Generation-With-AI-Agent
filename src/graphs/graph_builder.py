32c12ff6c3e93710adbba87d50190eb10e63c916
sliitguy <it23259416@my.sliit.lk>
2025-06-27 19:28:24 +0530

    correcting errors

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -1,29 +1,27 @@
-
 from langgraph.graph import StateGraph, START, END
 from src.llms.groqllm import GroqLLM
 from src.states.blogstate import BlogState
 from src.nodes.blog_node import BlogNode
 
 class GraphBuilder:
-    def __init__(self, llm):
-        self.llm = llm
-        self.graph = StateGraph(BlogState)
+    def __init__(self,llm):
+        self.llm=llm
+        self.graph=StateGraph(BlogState)
 
     def build_topic_graph(self):
         """
-        Build a graph to generate blogs based on topics
+        Build a graph to generate blogss based on topic
         """
-
-        self.blog_node_obj = BlogNode(self.llm)
-
+        self.blog_node_obj=BlogNode(self.llm)
+        print(self.llm)
         ## Nodes
         self.graph.add_node("title_creation", self.blog_node_obj.title_creation)
-        self.graph.add_node("content_generation", self.blog_node_obj.content_generation)
-        
-        # Edges
-        self.graph.add_edge(START, "title_creation")
-        self.graph.add_edge("title_creation", "content_generation")
-        self.graph.add_edge("content_generation", END)
+        self.graph.add_node("content_generation",self.blog_node_obj.content_generation)
+
+        ## Edges
+        self.graph.add_edge(START,"title_creation")
+        self.graph.add_edge("title_creation","content_generation")
+        self.graph.add_edge("content_generation",END)
 
         return self.graph
     
@@ -31,51 +29,48 @@ class GraphBuilder:
         """
         Build a graph for blog generation with inputs topic and language
         """
-
         self.blog_node_obj=BlogNode(self.llm)
         print(self.llm)
-
-        # Nodes
+        ## Nodes
         self.graph.add_node("title_creation", self.blog_node_obj.title_creation)
-        self.graph.add_node("content_generation", self.blog_node_obj.content_generation)
-        self.graph.add_node("tamil_translation", lambda state: self.blog_node_obj.translation({**state, "current_language": "tamil"}))
-        self.graph.add_node("sinhala_translation", lambda state: self.blog_node_obj.translation({**state, "current_language": "sinhala"}))
-        self.graph.add_node("route", self.blog_node_obj.route)
+        self.graph.add_node("content_generation",self.blog_node_obj.content_generation)
+        self.graph.add_node("hindi_translation",lambda state: self.blog_node_obj.translation({**state, "current_language": "hindi"}))
+        self.graph.add_node("french_translation",lambda state: self.blog_node_obj.translation({**state, "current_language": "french"}))
+        self.graph.add_node("route",self.blog_node_obj.route)
 
-        ## Edges and conditional edges
+        ## edges and conditional edges
         self.graph.add_edge(START, "title_creation")
         self.graph.add_edge("title_creation", "content_generation")
         self.graph.add_edge("content_generation", "route")
 
-        ## Conditional edge
+        ## conditional edge
         self.graph.add_conditional_edges(
             "route",
-            self.blog_node_obj.route_decision, 
+            self.blog_node_obj.route_decision,
             {
-                "tamil": "tamil_translation",
-                "sinhala": "sinhala_translation"
+                "hindi":"hindi_translation",
+                "french":"french_translation"
             }
         )
-        self.graph.add_edge("tamil_translation", END)
-        self.graph.add_edge("sinhala_translation", END)
-        
+        self.graph.add_edge("hindi_translation",END)
+        self.graph.add_edge("french_translation",END)
         return self.graph
 
-
     
-    def setup_graph(self, usecase):
-        if usecase =="topic":
+    def setup_graph(self,usecase):
+        if usecase=="topic":
             self.build_topic_graph()
-        if usecase == "language":
+        if usecase=="language":
+            print("Language block")
             self.build_language_graph()
 
-
         return self.graph.compile()
+    
 
+## Below code is for the langsmith langgraph studio
+llm=GroqLLM().get_llm()
 
-## Below code is for langsmith langgraph studio
-llm = GroqLLM().get_llm()
-
-# get the graph
+## get the graph
 graph_builder=GraphBuilder(llm)
-graph = graph_builder.build_language_graph().compile()
+graph=graph_builder.build_language_graph().compile()
+
--- src/nodes/blog_node.py
+++ src/nodes/blog_node.py
@@ -1,46 +1,46 @@
-
 from src.states.blogstate import BlogState
 from langchain_core.messages import SystemMessage, HumanMessage
 from src.states.blogstate import Blog
 
 class BlogNode:
     """
-    A class to represent the blog node
+    A class to represent he blog node
     """
 
-    def __init__(self, llm):
-        self.llm = llm
+    def __init__(self,llm):
+        self.llm=llm
+
     
-    def title_creation(self, state:BlogState):
+    def title_creation(self,state:BlogState):
         """
-        Create the title for the blog
+        create the title for the blog
         """
 
         if "topic" in state and state["topic"]:
             prompt="""
-                    You are a expert blog content writer. Use Markdown formatting. Generate
-                    a blog title for the {topic}. This title should be creative and SEO friendly.
-                
-                    """
+                   You are an expert blog content writer. Use Markdown formatting. Generate
+                   a blog title for the {topic}. This title should be creative and SEO friendly
+
+                   """
             
-            system_message=prompt.format(topic=state["topic"])
-            response =self.llm.invoke(system_message)
-
-            return {"blog": {"title": response.content}}
-
-    def content_generation(self, state:BlogState):
+            sytem_message=prompt.format(topic=state["topic"])
+            print(sytem_message)
+            response=self.llm.invoke(sytem_message)
+            print(response)
+            return {"blog":{"title":response.content}}
+        
+    def content_generation(self,state:BlogState):
         if "topic" in state and state["topic"]:
             system_prompt = """You are expert blog writer. Use Markdown formatting.
             Generate a detailed blog content with detailed breakdown for the {topic}"""
-            system_message=system_prompt.format(topic=state["topic"])
-            response=self.llm.invoke(system_message)
-            return {"blog": {"title": state["blog"]["title"], "content": response.content}}
-
-    def translation(self, state:BlogState):
+            system_message = system_prompt.format(topic=state["topic"])
+            response = self.llm.invoke(system_message)
+            return {"blog": {"title": state['blog']['title'], "content": response.content}}
+        
+    def translation(self,state:BlogState):
         """
-        Translate the content to the specified language
+        Translate the content to the specified language.
         """
-
         translation_prompt="""
         Translate the following content into {current_language}.
         - Maintain the original tone, style, and formatting.
@@ -48,28 +48,28 @@ class BlogNode:
 
         ORIGINAL CONTENT:
         {blog_content}
-        
+
         """
-
+        print(state["current_language"])
         blog_content=state["blog"]["content"]
-
         messages=[
             HumanMessage(translation_prompt.format(current_language=state["current_language"], blog_content=blog_content))
-        ]
-        translation_content=self.llm.with_structured_output(Blog).invoke(messages)
 
-    
+        ]
+        transaltion_content = self.llm.with_structured_output(Blog).invoke(messages)
+        return {"blog": {"content": transaltion_content}}
+
     def route(self, state: BlogState):
-        return {"current_language": state["current_language"]}
+        return {"current_language": state['current_language'] }
     
 
     def route_decision(self, state: BlogState):
         """
         Route the content to the respective translation function.
         """
-        if state["current_language"] == "tamil":
-            return "tamil"
-        elif state["current_language"] == "sinhala":
-            return "sinhala"
+        if state["current_language"] == "hindi":
+            return "hindi"
+        elif state["current_language"] == "french": 
+            return "french"
         else:
-            return state["current_language"]
+            return state['current_language']
